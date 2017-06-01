3f25536ea570e01bf221963855ca11fb31f20fed
Jean-Francois Rioux <jean-francoisrioux@MacBook-Pro-de-Jean-Francois.local>
2017-06-01 16:00:43 -0400

    ok

--- contracts/pool.sol
+++ contracts/pool.sol
@@ -6,12 +6,8 @@ import "./Member.sol";
 contract Pool {
 
   uint public total;
-  mapping (bytes32 => uint) public members;
   address[] public membersList;
 
-  function pool() {
-
-  }
 
   function enrollMore(string member, uint amount) {
     var addedMember = new Member();
@@ -31,38 +27,13 @@ contract Pool {
     minAmountTotal(loss);
   }
 
-  function processClaim(string member, uint loss){
-    // substract amount from members money
-    // ajust pool total
-    var memberID = sha256(member);
-    minAmountMember(memberID, loss);
-    minAmountTotal(loss);
-  }
-
-  // ***** DEPRECATED ******
-  function enroll(string member, uint amount){
-   // add member and his amount
-   // calculate new total
-   var memberID = sha256(member);
-   addAmountMember(memberID, amount);
-   addAmountTotal(amount);
-  }
-
   function addAmountTotal(uint amountToAdd) {
     // add a call to split with insurer partner
     total = total + amountToAdd;
   }
 
-  function addAmountMember(bytes32 member, uint amount) {
-    members[member] = members[member] + amount;
-  }
-
   function minAmountTotal(uint amountToSubstract) {
     total = total - amountToSubstract;
-  }
-
-  function minAmountMember(bytes32 member, uint loss) {
-    members[member] = members[member] - loss;
   }
 
   function getTotal() constant returns (uint) {
