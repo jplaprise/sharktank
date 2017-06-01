474247e924a5d14163060e88cdc4e481663170bb
julien-pierre.laprise.1@etsmtl.net Primary  Public <julien-pierre.laprise.1@etsmtl.net>
2017-06-01 11:47:02 -0400

    fini creation projet web

--- gs-spring-boot-master/complete/src/main/java/hello/HelloController.java
+++ gs-spring-boot-master/complete/src/main/java/hello/HelloController.java
@@ -1,13 +1,28 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @RestController
 public class HelloController {
-    
+
+    @Autowired
+    private WebBlockChain block;
+
     @RequestMapping("/")
     public String index() {
+
+
+
+        try {
+            System.out.println("this is the version : " + block.getClientVersion());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "Greetings from Spring Boot!";
     }
     
